7790e0061e16a7b32d1d3dae11a1e371daff483a
fumikito <guy@hametuha.com>
2023-12-26 14:20:52 +0900

    Fix library to check lint.

--- bin/build.sh
+++ bin/build.sh
@@ -4,8 +4,12 @@ set -e
 
 # Build files
 composer install --no-dev --prefer-dist
+php bin/replacer.php
+
+# NPM packages.
 npm install
 npm run package
+
 # Make Readme
 echo 'Generate readme.'
 curl -L https://raw.githubusercontent.com/fumikito/wp-readme/master/wp-readme.php | php
